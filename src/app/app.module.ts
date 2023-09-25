0c259f495249ee0862533b35f9496e97f97aebbc
AngieValverdeSalazar <89101151+AngieValverdeSalazar@users.noreply.github.com>
2023-09-25 01:40:01 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditProfileComponent
+
   ],
   imports: [
     BrowserModule
