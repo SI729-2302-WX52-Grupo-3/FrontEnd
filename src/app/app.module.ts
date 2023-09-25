dc80c006054c68e3b3589e0b68be114a1f678380
AngieValverdeSalazar <89101151+AngieValverdeSalazar@users.noreply.github.com>
2023-09-25 01:55:41 -0500

    edit profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    EditProfileComponent,
 
   ],
   imports: [
