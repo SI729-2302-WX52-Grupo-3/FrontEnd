b48709649c9dbcb34357c09fdde1846fce07c90e
AlfredoFarro <u201911924@upc.edu.pe>
2023-09-28 02:21:34 -0500

    Adding starting screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { HomeComponent } from './views/home/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LogInComponent } from './components/log-in/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    HomeComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
