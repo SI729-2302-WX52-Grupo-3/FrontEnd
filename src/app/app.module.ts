a7997752faaa6b6412698b1b5b021a86fa5765ea
AlfredoFarro <u201911924@upc.edu.pe>
2023-09-28 03:08:32 -0500

    Adding changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
-import { HomeComponent } from './views/home/home/home.component';
+import { HomeComponent } from './views/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LogInComponent } from './components/log-in/log-in/log-in.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent,
     EditProfileComponent,
     HomeComponent,
-    LogInComponent
+    ToolbarComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
