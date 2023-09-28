afcdd755ebb7d1bed50d2c82cb1e0179d3842827
AlfredoFarro <u201911924@upc.edu.pe>
2023-09-28 04:04:39 -0500

    Adding Login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./views/home/home.component";
+import {EditProfileComponent} from "./components/edit-profile/edit-profile.component";
+import {LogInComponent} from "./views/patients/log-in/log-in.component"
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'patientsLogIn', component: LogInComponent },
+
+
+
+
+
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LogInCardComponent } from './components/log-in-card/log-in-card.component';
+import { LogInComponent } from './views/patients/log-in/log-in.component';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    LogInCardComponent,
+    LogInComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
